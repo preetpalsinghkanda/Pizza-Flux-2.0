ebbbd0aff170d4ad4f8f8553bc062027dd2803a2
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-04-14 20:36:53 +0530

    added responsiveness in ReadyOrder

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ const App = () => {
   return (
     <div>
         <NavBar/>
-          <div className='flex flex-col gap-35 my-15 '><Hero/><WhyPizzaFlux/></div>
+          <div className='flex flex-col gap-35 my-15 '><Hero/><WhyPizzaFlux/> <ReadyOrder/> </div>
         
-       {/* 
-        <ReadyOrder/> */}
+       
+       
        
     </div>
   )
--- src/Components/ReadyOrder.jsx
+++ src/Components/ReadyOrder.jsx
@@ -3,11 +3,11 @@ import cook from "/cook.png";
 
 const ReadyOrder = () => {
   return (
-    <div className=" m-auto w-max readyorder flex ">
-      <img src={cook} alt="" className="h-100" />
-      <div className="  flex gap-4 flex-col justify-center items-center px-12 py-12 rounded-3xl">
+    <div className="m-auto max-w-md md:max-w-2xl readyorder flex flex-col md:flex-row items-center gap-4">
+      <img src={cook} alt="" className="h-100 w-auto" />
+      <div className="  flex gap-4 flex-col justify-center items-center rounded-3xl">
         <h3 className="text-[#ff5f2e] text-4xl font-[800]">Ready to Order ?</h3>
-        <p className="text-[#ffffffb7]">
+        <p className="text-[#ffffffb7]  break-normal px-3 text-center">
           Create your account and get your first delivery free!
         </p>
         <button className="bg-[#b32c02] text-white px-6 py-3 rounded-xl text-lg ">
