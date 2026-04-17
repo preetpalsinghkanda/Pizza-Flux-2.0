57ed6116087f8ce8ffa8c8d688ce7f09480b65b8
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-04-17 17:32:16 +0530

    implemented responsiveness in main pizza menu page

--- src/Components/PizzaMenu/PizzaMenu.jsx
+++ src/Components/PizzaMenu/PizzaMenu.jsx
@@ -6,20 +6,20 @@ import chickenNewPizza from "/chickennew.png";
 
 const PizzaMenu = () => {
   return (
-    <div className=" text-white m-auto max-w-6xl pizza-main flex flex-col gap-2">
+    <div className=" text-white m-auto max-w-7xl pizza-main flex flex-col gap-2 px-6">
       <div className="relative top-12">
-        <h3 className="text-7xl">
-          Our Signature <span className="text-[#ff2600]">Pizzas</span>
+        <h3 className="lg:text-7xl md:text-6xl sm:text-5xl text-5xl">
+          Our Signature <span className="text-[#ff2600] text-6xl sm:text-6xl md:text-7xl lg:text-8xl">Pizzas</span>
         </h3>
-        <p className="text-xl text-[#ADABAA]">
+        <p className="lg:text-xl md:text-lg sm:text-lg text-[#ADABAA]">
           Hot. Fresh. Irresistible. Each slice is a masterpiece crafted in our
           wood fired hearth at 450°C.
         </p>
       </div>
 
-      <div className="flex gap-6 ">
+      <div className="flex gap-6  md:flex-col lg:flex-row  sm:flex-col flex-col">
         <div className=" flex flex-col items-center ">
-          <img src={chickenPizza} alt="" className="h-60 relative top-30" />
+          <img src={chickenPizza} alt="" className="h-60 relative top-30  pizza-card-img" />
           <div className=" pt-35 px-6 rounded-4xl pb-6 gap-6 flex flex-col bg-[#131313]">
             <div className="flex flex-col gap-3">
               <h5 className="text-3xl font-extrabold">Chicken blast</h5>
@@ -40,8 +40,8 @@ const PizzaMenu = () => {
         </div>
 
         <div className=" flex flex-col items-center ">
-          <img src={pepperoniPizza} alt="" className="h-60 relative top-30" />
-          <div className=" pt-35 px-6 rounded-4xl pb-6 gap-6 flex flex-col bg-[#131313]">
+          <img src={pepperoniPizza} alt="" className="h-60 relative top-30 pizza-card-img" />
+          <div className=" pt-35 px-6 rounded-4xl pb-6 gap-6 flex flex-col bg-[#131313]  ">
             <div className="flex flex-col gap-3">
               <h5 className="text-3xl font-extrabold">Pepperoni Feast</h5>
               <p className="text-[#ffffff84]">
@@ -61,8 +61,8 @@ const PizzaMenu = () => {
         </div>
 
         <div className=" flex flex-col items-center ">
-          <img src={chickenNewPizza} alt="" className="h-60 relative top-30" />
-          <div className=" pt-35 px-6 rounded-4xl pb-6 gap-6 flex flex-col bg-[#131313]">
+          <img src={chickenNewPizza} alt="" className="h-60 relative top-30 pizza-card-img" />
+          <div className=" pt-35 px-6 rounded-4xl pb-6 gap-6 flex flex-col bg-[#131313] ">
             <div className="flex flex-col gap-3">
               <h5 className="text-3xl font-extrabold">Chicken Tikka Delight</h5>
               <p className="text-[#ffffff84]">
