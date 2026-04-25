cbf25257791907f8a17fa69302beb05b34afc8ca
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-04-25 21:12:51 +0530

    project completed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,50 +4,55 @@ import PizzaContext from "../Context/Context";
 import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  const navigate = useNavigate()
- 
+  const navigate = useNavigate();
+
   const [isNavBarOpen, setIsNavBar] = useState(false);
   return (
     <>
       <div className="bg-[#111110]   flex justify-center border-b-1 border-[#ffffff13]">
         <div className="navbar flex justify-around lg:justify-between items-center w-full py-2 max-w-6xl m-auto">
-          <div className="flex items-center gap-2 cursor-pointer" onClick={()=>navigate("/")}>
+          <div
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={() => navigate("/")}
+          >
             <img src={PizzaLogo} alt="" className="md:h-15 h-10 " />
-            <span className="md:text-3xl text-xl text-[#ff5f2e]">PizzaFlux 2.0</span>
+            <span className="md:text-3xl text-xl text-[#ff5f2e]">
+              PizzaFlux 2.0
+            </span>
           </div>
 
           <div className=" flex-row items-center gap-4 hidden md:flex">
-             <div
-  onClick={() => navigate("/menu")}
-  className={`cursor-pointer ${
-    location.pathname === "/menu"
-      ? "bg-[#f6a823] text-black"
-      : "text-white"
-  } hover:bg-[#727272] hover:text-white px-2 py-1 rounded-lg flex items-center gap-2`}
->
+            <div
+              onClick={() => navigate("/menu")}
+              className={`cursor-pointer ${
+                location.pathname === "/menu"
+                  ? "bg-[#f6a823] text-black"
+                  : "text-white"
+              } hover:bg-[#727272] hover:text-white px-2 py-1 rounded-lg flex items-center gap-2`}
+            >
               <span className="material-symbols-outlined ">menu_book_2</span>
             </div>
-             <div
-  onClick={() => navigate("/cart")}
-  className={`cursor-pointer ${
-    location.pathname === "/cart"
-      ? "bg-[#f6a823] text-black"
-      : "text-white"
-  } hover:bg-[#727272] hover:text-white px-2 py-1 rounded-lg flex items-center gap-2`}
->
+            <div
+              onClick={() => navigate("/cart")}
+              className={`cursor-pointer ${
+                location.pathname === "/cart"
+                  ? "bg-[#f6a823] text-black"
+                  : "text-white"
+              } hover:bg-[#727272] hover:text-white px-2 py-1 rounded-lg flex items-center gap-2`}
+            >
               <span className="material-symbols-outlined">shopping_cart</span>
             </div>
-           <div
-  onClick={() => navigate("/signup")}
-  className={`cursor-pointer ${
-    location.pathname === "/signup"
-      ? "bg-[#f6a823] text-black"
-      : "text-white"
-  } hover:bg-[#727272] hover:text-white px-2 py-1 rounded-lg flex items-center gap-2`}
->
-  <span className="material-symbols-outlined">person_edit</span>
-  <span className="text-lg">Signup</span>
-</div>
+            <div
+              onClick={() => navigate("/signup")}
+              className={`cursor-pointer ${
+                location.pathname === "/signup"
+                  ? "bg-[#f6a823] text-black"
+                  : "text-white"
+              } hover:bg-[#727272] hover:text-white px-2 py-1 rounded-lg flex items-center gap-2`}
+            >
+              <span className="material-symbols-outlined">person_edit</span>
+              <span className="text-lg">Signup</span>
+            </div>
           </div>
 
           <div
@@ -59,7 +64,9 @@ const NavBar = () => {
                 dehaze
               </span>
             ) : (
-              <span className="material-symbols-outlined text-white">close</span>
+              <span className="material-symbols-outlined text-white">
+                close
+              </span>
             )}
           </div>
         </div>
@@ -67,15 +74,24 @@ const NavBar = () => {
 
       {isNavBarOpen && (
         <div className="small-navbar border md:hidden flex absolute right-0  flex-col gap-3 items-center px-2 py-2">
-          <div className="text-white items-center flex bg-[#20201f] px-4 py-2 rounded-xl gap-2">
+          <div
+            onClick={() => navigate("/menu")}
+            className="text-white items-center flex bg-[#20201f] px-4 py-2 rounded-xl gap-2"
+          >
             Menu <span className="material-symbols-outlined ">menu_book_2</span>
           </div>
-          <div className="text-white items-center flex bg-[#20201f] px-4 py-2 rounded-xl gap-2">
+          <div
+            onClick={() => navigate("/cart")}
+            className="text-white items-center flex bg-[#20201f] px-4 py-2 rounded-xl gap-2"
+          >
             {" "}
             Cart{" "}
             <span className="material-symbols-outlined">shopping_cart</span>
           </div>
-          <div className="text-white items-center flex bg-[#20201f] px-4 py-2 rounded-xl gap-2">
+          <div
+            onClick={() => navigate("/signup")}
+            className="text-white items-center flex bg-[#20201f] px-4 py-2 rounded-xl gap-2"
+          >
             Signup{" "}
             <span className="material-symbols-outlined ">person_edit</span>
           </div>
